c52fd433cb73e7963e0a8fcbbe0b6d1b5bbb78c4
Manonbe89 <manon.bettstatter@gmail.com>
2025-06-02 18:45:04 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
 #include "Player.h"
+#include "Monopoly.h"
 
 int main(void) {
-	std::cout << "Start of the game" << std::endl; 
+	std::cout << "Start of the game" << std::endl;
 	std::cout << "How many players are you ?" << std::endl;
 	int nb_players;
 	std::cin >> nb_players;
 
-	std::cout << "What are your names ?";
-	//if (nb_players == 2) {
-		//for (int i = 0; i < nb_players; i++) {
-			std::string name;
-			std::cin >> name;
-			Player player1(name, 31, 15);
-		//}
-	//}
-			player1.getMoney();
+	std::vector<Player> player;
+	std::cout << "What are your names ?" << std::endl;
+	for (int i = 0; i < nb_players; i++) {
+		std::string name;
+		std::cin >> name;
+		if (nb_players == 2) {
+			player.emplace_back(name, 31, 15);
+		}
+		else {
+			player.emplace_back(name, 31, 10);
+		}
+		
+	}
 }
